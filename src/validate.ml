3da0f08bf6571637d51214ae97c66333d26874dc
Emma Ndoliam <endoliam@z1r11p1.42lyon.fr>
2026-07-06 12:12:10 +0200

    add validation inputs

--- src/validate.ml
+++ src/validate.ml
@@ -9,17 +9,36 @@ let rec has_duplicates xs =
   | [] | [_] -> false
   | x :: rest -> List.mem x rest || has_duplicates rest
 
+let rec input_exists (input: string) (input_map: (string * string) list) =
+  match input_map with
+  | [] -> false
+  | (_, token) :: rest -> token = input || input_exists input rest
+
 let validate_input (input_map: (string * string) list) =
   let inputs = List.map fst input_map in
   ensure (not (has_duplicates inputs)) "Duplicate inputs found in input_map"
 
-let validate_combos (combos: (string list * string)list) =
+let validate_combos_name (combos: (string list * string)list) =
   let combo_names = List.map snd combos in
   ensure (not (has_duplicates combo_names)) "Duplicate combo names found in combos"
 
+let validate_combos_inputs (combos: (string list * string) list) (input_map: (string * string) list) =
+  let rec aux combos =
+    match combos with
+    | [] -> ()
+    | (inputs, _) :: rest ->
+      List.iter (fun input ->
+        ensure (input_exists input input_map) ("Input '" ^ input ^ "' in combos does not exist in input_map")
+      ) inputs;
+      aux rest
+  in
+  aux combos
+
+let validate_combos_inputs_
 let validate_automaton (parsed: Automaton.ParsingTypes.parsed_grammar): unit = 
   try
     validate_input parsed.input_map;
-    validate_combos parsed.combos;
+    validate_combos_name parsed.combos;
+    validate_combos_inputs parsed.combos parsed.input_map;
   with Validation_error message ->
     failwith message
